367c7fe0170f4e36ff0752f530c99d62d4a76307
Snir Shalom <snir2015shalom@gmail.com>
2023-03-26 23:50:24 +0300

    added material ui + useref does not work bug

--- src/EventRegister.jsx
+++ src/EventRegister.jsx
@@ -1,4 +1,9 @@
 import { useState, useRef } from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import TextField from "@mui/material/TextField";
 
 const lacture = {
   workshopTitle: "the mistory of creepy",
@@ -9,13 +14,12 @@ const lacture = {
     src: "https://res.cloudinary.com/ontopo/image/upload/q_auto:eco,c_crop,x_0,y_85,w_1024,h_379/w_950,c_scale/v1671968906/assets/62399069302c8c1fa0a48f4e/6239906b302c8c1fa0a48f51/2.png",
     alt: "some ALT...",
   },
-  workshopDate: "DATE",
+  workshopDate: "Date",
   workshopTeacher: "Avi Nahoom",
   workshopEventContant:
     "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, sequi culpa iure molestiae ad omnis repudiandae itaque? Vel omnis recusandae, eaque ex dicta sint mollitia velit ratione maiores quia? Enim dolorum nulla facilis sit velit modi debitis nostrum distinctio, laudantium doloremque error odit neque ipsum? Impedit, officiis excepturi. Perferendis quia id doloremque quas recusandae a maxime porro laboriosam itaque placeat dolor iure dolorem, eveniet sit provident hic molestiae perspiciatis debitis necessitatibus eos possimus molestias cum error sint? Nobis doloremque vero culpa sunt facilis, perferendis ad. Eum aut fugiat eligendi veritatis, dolorem assumenda in dolor totam pariatur soluta tenetur, aliquam saepe.",
 };
 
-// change to useRef
 const EventReginster = () => {
   const [userList, setUserList] = useState([
     {
@@ -54,27 +58,57 @@ const EventReginster = () => {
       <img src={lacture.workshopImage.src} alt={lacture.workshopImage.alt} />
       <h2>{lacture.workshopDesc}</h2>
       {lacture.workshopHashtags.map((tag) => {
-        return <span>#{tag}</span>;
+        return (
+          <ButtonGroup
+            variant="contained"
+            aria-label="outlined primary button group"
+            className="workShop__tags"
+          >
+            #{tag}
+          </ButtonGroup>
+        );
       })}
-      <p>{lacture.workshopDate}</p>
-      <p>{lacture.workshopTeacher}</p>
-      <p>{lacture.workshopEventContant}</p>
 
-      <form>
-        <label>
-          Name:
-          <input type="text" ref={inputRef_name} />
-        </label>
-        <label>
-          phone Number:
-          <input type="number" ref={inputRef_phoneNumber} />
-        </label>
-        <label>
-          Email:
-          <input type="email" ref={inputRef_email} />
-        </label>
-      </form>
-      <button onClick={() => addRegisteredList()}>Register Now!</button>
+      {/* date and teacher */}
+
+      <Box sx={{ "& > :not(style)": { m: 1 } }}>
+        <p>{lacture.workshopEventContant}</p>
+        <Fab variant="extended">
+          <p sx={{ mr: 1 }} className="workShop__detials">
+            At: {lacture.workshopDate}{" "}
+          </p>
+        </Fab>
+        <Fab variant="extended">
+          <p sx={{ mr: 1 }}>By: {lacture.workshopTeacher}</p>
+        </Fab>
+      </Box>
+
+      {/* input field */}
+
+      <Box className="input-field">
+        <TextField
+          label="Name"
+          variant="outlined"
+          type={"text"}
+          ref={inputRef_name}
+        />
+        <TextField
+          label="Phone"
+          variant="outlined"
+          type={"nuber"}
+          ref={inputRef_phoneNumber}
+        />
+        <TextField
+          label="email"
+          variant="outlined"
+          type={"email"}
+          ref={inputRef_email}
+        />
+        <Button variant="contained" onClick={() => addRegisteredList()}>
+          Register Now!
+        </Button>
+      </Box>
+
       {userList.map((user) => (
         <h4>
           {user.name}

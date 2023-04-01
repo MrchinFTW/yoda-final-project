53f1777e42318654a7dff2eebcae24008b46d428
Snir Shalom <snir2015shalom@gmail.com>
2023-04-01 17:25:45 +0300

    replaced mock data with useWorkshop hook

--- src/EventRegister.jsx
+++ src/EventRegister.jsx
@@ -5,6 +5,8 @@ import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import TextField from "@mui/material/TextField";
 import SendIcon from "@mui/icons-material/Send";
+import { useParams } from "react-router";
+import useWorkshop from "./Hooks/useWorkshop";
 
 const getCurrentDate = () => {
   const dateObj = new Date();
@@ -15,29 +17,8 @@ const getCurrentDate = () => {
   return day + "/" + month + "/" + year;
 };
 
-const lacture = {
-  workshopTitle: "the mistory of creepy",
-  workshopDesc: "this is a very creepy lecture...",
-  workshopMiniDesc: "shady lecturer to a shady lecture",
-  workshopHashtags: ["creep", "shady", "strong heart"],
-  workshopImage: {
-    src: "https://res.cloudinary.com/ontopo/image/upload/q_auto:eco,c_crop,x_0,y_85,w_1024,h_379/w_950,c_scale/v1671968906/assets/62399069302c8c1fa0a48f4e/6239906b302c8c1fa0a48f51/2.png",
-    alt: "some ALT...",
-  },
-  workshopDate: getCurrentDate(),
-  workshopTeacher: "Avi Nahoom",
-  workshopEventContant:
-    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur, sequi culpa iure molestiae ad omnis repudiandae itaque? Vel omnis recusandae, eaque ex dicta sint mollitia velit ratione maiores quia? Enim dolorum nulla facilis sit velit modi debitis nostrum distinctio, laudantium doloremque error odit neque ipsum? Impedit, officiis excepturi. Perferendis quia id doloremque quas recusandae a maxime porro laboriosam itaque placeat dolor iure dolorem, eveniet sit provident hic molestiae perspiciatis debitis necessitatibus eos possimus molestias cum error sint? Nobis doloremque vero culpa sunt facilis, perferendis ad. Eum aut fugiat eligendi veritatis, dolorem assumenda in dolor totam pariatur soluta tenetur, aliquam saepe.",
-};
-
 const EventRegister = () => {
-  const [userList, setUserList] = useState([
-    {
-      name: "shimon",
-      phoneNumber: "0501111111",
-      email: "shimon@hotmail",
-    },
-  ]);
+  const [userList, setUserList] = useState([]);
 
   let inputRef_name = useRef();
   let inputRef_phoneNumber = useRef();
@@ -62,76 +43,83 @@ const EventRegister = () => {
     inputRef_email.current.value = "";
   };
 
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h1>{lacture.workshopTitle}</h1>
-      <img src={lacture.workshopImage.src} alt={lacture.workshopImage.alt} />
-      <h2>{lacture.workshopDesc}</h2>
-      {lacture.workshopHashtags.map((tag) => {
-        return (
-          <ButtonGroup
+  const paramId = useParams();
+
+  const { data: lectures, isLoading, isError } = useWorkshop();
+
+  if (lectures) {
+    const currentLecture = lectures.find(
+      (lecture) => lecture._id === paramId.id
+    );
+    return (
+      <div style={{ textAlign: "center" }}>
+        <img
+          src={currentLecture.workshopImage.src}
+          alt={currentLecture.workshopImage.alt}
+        />
+        <h1>{currentLecture.workshopTitle}</h1>
+        <h2>{currentLecture.workshopDesc}</h2>
+        {currentLecture.workshopHashtags.map((tag) => {
+          return (
+            <ButtonGroup
+              variant="contained"
+              aria-label="outlined primary button group"
+              className="workShop__tags"
+            >
+              #{tag}
+            </ButtonGroup>
+          );
+        })}
+
+        <p>{currentLecture.workshopMiniDesc}</p>
+
+        {/* date and teacher */}
+
+        <div className="dateAndTeacher">
+          <div>Teacher: {currentLecture.workshopTeacher}</div>
+          <div>Date: {currentLecture.workshopDate}</div>
+        </div>
+
+        {/* input field */}
+
+        <Box className="input-field">
+          <TextField
+            label="Name"
+            variant="outlined"
+            type={"text"}
+            inputRef={inputRef_name}
+          />
+          <TextField
+            label="Phone"
+            variant="outlined"
+            type={"nuber"}
+            inputRef={inputRef_phoneNumber}
+          />
+          <TextField
+            label="email"
+            variant="outlined"
+            type={"email"}
+            inputRef={inputRef_email}
+          />
+          <Button
             variant="contained"
-            aria-label="outlined primary button group"
-            className="workShop__tags"
+            endIcon={<SendIcon />}
+            onClick={() => addRegisteredList()}
           >
-            #{tag}
-          </ButtonGroup>
-        );
-      })}
+            Register Now
+          </Button>
+        </Box>
 
-      {/* date and teacher */}
-
-      <Box sx={{ "& > :not(style)": { m: 1 } }}>
-        <p>{lacture.workshopEventContant}</p>
-        <Fab variant="extended">
-          <p sx={{ mr: 1 }} className="workShop__detials">
-            At: {lacture.workshopDate}{" "}
-          </p>
-        </Fab>
-        <Fab variant="extended">
-          <p sx={{ mr: 1 }}>By: {lacture.workshopTeacher}</p>
-        </Fab>
-      </Box>
-
-      {/* input field */}
-
-      <Box className="input-field">
-        <TextField
-          label="Name"
-          variant="outlined"
-          type={"text"}
-          inputRef={inputRef_name}
-        />
-        <TextField
-          label="Phone"
-          variant="outlined"
-          type={"nuber"}
-          inputRef={inputRef_phoneNumber}
-        />
-        <TextField
-          label="email"
-          variant="outlined"
-          type={"email"}
-          inputRef={inputRef_email}
-        />
-        <Button
-          variant="contained"
-          endIcon={<SendIcon />}
-          onClick={() => addRegisteredList()}
-        >
-          Register Now
-        </Button>
-      </Box>
-
-      {userList.map((user) => (
-        <h4>
-          {user.name}
-          {user.phoneNumber}
-          {user.email}
-        </h4>
-      ))}
-    </div>
-  );
+        {userList.map((user) => (
+          <h4>
+            {user.name}
+            {user.phoneNumber}
+            {user.email}
+          </h4>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default EventRegister;
--- src/Hooks/useWorkshop.js
+++ src/Hooks/useWorkshop.js
@@ -1,11 +1,11 @@
-import { useQuery } from 'react-query';
+import { useQuery } from "react-query";
 
 const useWorkshop = () => {
-	const { data, isLoading, isError } = useQuery('workshop-data', () => {
-		return fetch('data.json').then((res) => res.json());
-	});
+  const { data, isLoading, isError } = useQuery("workshop-data", () => {
+    return fetch("/data.json").then((res) => res.json());
+  });
 
-	return { data, isLoading, isError };
+  return { data, isLoading, isError };
 };
 
 export default useWorkshop;

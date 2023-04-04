ea383588a4599d5a437d882ff939eff1b255a17f
unknown <bati.mayer@gmail.com>
2023-04-04 20:31:07 +0300

    Added UserContext and UserProvider component with state and functions,timeouts as mock data

--- src/components/Login/UserContext.js
+++ src/components/Login/UserContext.js
@@ -1,22 +1,55 @@
-import React, { createContext, useState, useContext } from 'react'
+import React, { createContext, useContext, useState, useEffect, useMemo } from "react"
 
-export const UserContext = createContext()
-
-export const useUserData = () => useContext(UserContext)
+const UserContext = createContext()
 
 export const UserProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [isLoggingIn, setIsLoggingIn] = useState(false)
 
-    const toggleLoggedIn = () => setIsLoggedIn(!isLoggedIn)
+    // will log the user out after 10 min
+    useEffect(() => {
+        let timerId
+        if (isLoggedIn) {
+            timerId = setTimeout(() => {
+                setIsLoggedIn(false)
+            }, 600000)
+        }
+        return () => clearTimeout(timerId)
+    }, [isLoggedIn])
 
-    const value = {
+    const value = useMemo(() => ({
         isLoggedIn,
-        toggleLoggedIn,
-    }
+        isLoggingIn,
 
-    return (
-        <UserContext.Provider value={value}>
-            {children}
-        </UserContext.Provider>
-    )
+        userLoggingIn: () => {
+            if (!isLoggedIn && !isLoggingIn) {
+                setIsLoggingIn(true)
+
+                const timeoutId = setTimeout(() => {
+                    setIsLoggedIn(true)
+                    setIsLoggingIn(false)
+                }, 3000)
+
+                return () => clearTimeout(timeoutId)
+            }
+        },
+        userLogout: () => {
+            if (isLoggedIn) {
+                setIsLoggedIn(false)
+            }
+        },
+
+        sayHello: (userName) => {
+            alert('Hello' + userName)
+        },
+        logginError: (err) => {
+            console.log('error accured: ' + err)
+        }
+
+    }), [isLoggedIn, isLoggingIn])
+
+    return <UserContext.Provider value={value}>
+        {children}
+    </UserContext.Provider>
 }
+

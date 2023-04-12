ce568cf2a92c3b8b54bb96353025b41b376862bc
unknown <bati.mayer@gmail.com>
2023-04-12 20:13:23 +0300

    added user context

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Login from './Login';
-import EventRegister from './EventRegister';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import WorkshopAdminPage from './Pages/WorkshopAdminPage';
-import HomeLayout from './Components/HomeLayout';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import Login from './Login'
+import EventRegister from './EventRegister'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import WorkshopAdminPage from './Pages/WorkshopAdminPage'
+import HomeLayout from './Components/HomeLayout'
+import { UserProvider } from './Components/Login/UserContext'
 
-const queryClient = new QueryClient();
+
+const queryClient = new QueryClient()
 
 function App() {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<div className='App'>
-				<HomeLayout>
-					<Routes>
-						<Route path='/' element={<Home />} />
-						<Route path='/login' element={<Login />} />
-						<Route path='/event/:id' element={<EventRegister />} />
-						<Route path='/add-event' element={<WorkshopAdminPage />} />
-					</Routes>
-				</HomeLayout>
-			</div>
-		</QueryClientProvider>
-	);
+		<UserProvider>
+			<QueryClientProvider client={queryClient}>
+				<div className='App'>
+					<HomeLayout>
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/login' element={<Login />} />
+							<Route path='/event/:id' element={<EventRegister />} />
+							<Route path='/add-event' element={<WorkshopAdminPage />} />
+						</Routes>
+					</HomeLayout>
+				</div>
+			</QueryClientProvider>
+		</UserProvider>
+	)
 }
 
-export default App;
+export default App
--- src/components/Login/UserContext.js
+++ src/components/Login/UserContext.js
@@ -1,55 +1,74 @@
-import React, { createContext, useContext, useState, useEffect, useMemo } from "react"
+import React, { createContext, useContext, useEffect, useState } from "react"
 
 const UserContext = createContext()
 
 export const UserProvider = ({ children }) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
-    const [isLoggingIn, setIsLoggingIn] = useState(false)
+    const [isLoggedin, setIsLoggedin] = useState(false)
+    const [isLoggingin, setIsLoggingin] = useState(false)
+    const [userData, setUserData] = useState({
+        firstName: null,
+        lastName: null
+    })
 
-    // will log the user out after 10 min
-    useEffect(() => {
-        let timerId
-        if (isLoggedIn) {
-            timerId = setTimeout(() => {
-                setIsLoggedIn(false)
-            }, 600000)
-        }
-        return () => clearTimeout(timerId)
-    }, [isLoggedIn])
-
-    const value = useMemo(() => ({
-        isLoggedIn,
-        isLoggingIn,
-
-        userLoggingIn: () => {
-            if (!isLoggedIn && !isLoggingIn) {
-                setIsLoggingIn(true)
-
-                const timeoutId = setTimeout(() => {
-                    setIsLoggedIn(true)
-                    setIsLoggingIn(false)
-                }, 3000)
-
-                return () => clearTimeout(timeoutId)
-            }
-        },
-        userLogout: () => {
-            if (isLoggedIn) {
-                setIsLoggedIn(false)
-            }
-        },
-
-        sayHello: (userName) => {
-            alert('Hello' + userName)
-        },
-        logginError: (err) => {
-            console.log('error accured: ' + err)
-        }
-
-    }), [isLoggedIn, isLoggingIn])
-
-    return <UserContext.Provider value={value}>
+    return <UserContext.Provider value={{ isLoggedin, setIsLoggedin, userData, setUserData, isLoggingin, setIsLoggingin }}>
         {children}
     </UserContext.Provider>
+
 }
 
+
+
+const useUserContext = () => {
+
+    const { isLoggedin, setIsLoggedin, isLoggingin, setIsLoggingin, userData, setUserData } = useContext(UserContext)
+
+    // This is mock, login will be done with an exturnal lib. 
+    const login = () => {
+        if (!isLoggedin) {
+            setIsLoggingin(true)
+
+
+            // To simulate the time that the login takes.
+            setTimeout(() => {
+                setIsLoggedin(true)
+                setIsLoggingin(false)
+
+            }, 3000)
+        }
+    }
+
+
+    const logout = () => {
+
+        if (isLoggedin) {
+            setIsLoggedin(false)
+            console.log("hello from logout func")
+        }
+    }
+
+
+
+    const userInfo = (firstName, lastName) => {
+        setUserData({
+            firstName,
+            lastName,
+        })
+        return { firstName, lastName }
+    }
+
+    const loginError = (err) => {
+        console.log('An error occurred: ' + err)
+    }
+
+
+    return {
+        login,
+        logout,
+        userInfo,
+        isLoggedin,
+        isLoggingin,
+        loginError
+    }
+}
+
+export default useUserContext
